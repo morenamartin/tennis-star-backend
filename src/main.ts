0abf72fc2bafecf170d690c44674a0e9d29c8507
morena-martin <morena@shaper.plus>
2026-02-09 11:49:11 -0300

    feat: Implement initial NestJS application setup, environment configuration, and CORS.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ["http://localhost:3000", "https://tennis-star.vercel.app"],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
   })
 
