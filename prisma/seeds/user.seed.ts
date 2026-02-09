beef5f5016aeedf88fa8678c58f46454a22d12fb
morena-martin <morena@shaper.plus>
2026-02-09 13:31:15 -0300

    Bycript en seed de usuario

--- prisma/seeds/user.seed.ts
+++ prisma/seeds/user.seed.ts
@@ -1,12 +1,19 @@
+import bcrypt from 'bcrypt'; // o 'bcryptjs'
+
 export async function seedUsers(prisma) {
+    // Definimos la cantidad de "salt rounds" para el encriptado
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash("Password123", saltRounds);
+
     await prisma.user.createMany({
         data: [
             { 
                 name: "Morena Martin",
                 email: "martinmorena269@gmail.com",
-                password: "Password123",
+                password: hashedPassword,
+                rememberMe: false
             },
         ],
         skipDuplicates: true,
-    })
+    });
 }
